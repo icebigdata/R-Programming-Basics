60b2c25b1a76d00f8d9892effddb4f73f999438b
icebigdata <fia4joy@yahoo.co.kr>
2018-07-09 01:23:33 +0900

    Add RDdemo-04,05,06 vua upload files

---
+++ RFdemo-06/07AnonymousFunction.R
@@ -0,0 +1,19 @@
+#Named function
+GetTotalMarks <- function(quiz.marks, viva.marks) { 
+  total.marks <- quiz.marks + viva.marks  
+  total.marks
+}
+
+#using named function
+do.call(GetTotalMarks,list(quiz.marks = c(70L, 75L, 80L, 85L), 
+                           viva.marks = c(7L, 5L, 8L, 6L)))
+
+#Using anonymous function
+do.call(function(quiz.marks, viva.marks) {
+                quiz.marks + viva.marks 
+},list(quiz.marks = c(70L, 75L, 80L, 85L), viva.marks = c(7L, 5L, 8L, 6L)))
+
+#익명함수 표현식
+(function(quiz.marks, viva.marks) {
+  quiz.marks + viva.marks 
+})(quiz.marks = c(70L, 75L, 80L, 85L), viva.marks = c(7L, 5L, 8L, 6L))
